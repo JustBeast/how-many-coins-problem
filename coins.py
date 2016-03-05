84c553574b5f9e2e0a4b67021f3cd535d4a78f20
Aditya Bhardwaj <adityabhardwaj222@gmail.com>
2016-03-05 15:56:42 -0800

    Create coins.py

---
+++ coins.py
@@ -0,0 +1,21 @@
+amount= float(input("Enter an amount: "))
+
+quarters=0 
+dimes= 0 
+nickels = 0
+pen= 0
+
+while amount >= .25: 
+  amount = amount - .25 
+  quarters += 1 
+while amount >= .1: 
+  amount = amount - .1 
+  dimes += 1 
+while amount >= .05: 
+    amount = amount - .05 
+    nickels += 1 
+while amount >= 0.01: 
+  amount = amount - .01
+  pen += 1
+
+if quarters > 0: print str(quarters)+ " quarters" if dimes > 0: print str(dimes)+ " dimes" if nickels > 0: print str(nickels)+ " nickels" if pen > 0: print str(pen) + " pennies"
